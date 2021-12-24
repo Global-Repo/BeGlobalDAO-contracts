dfd51234fae106a8b411faa23843825a88f951cf
Jordi Radev <>
2021-12-24 01:46:59 +0100

    no message

--- scripts/FullScript.js
+++ scripts/FullScript.js
@@ -52,7 +52,8 @@ async function main() {
     let stakingHelper;
     let globalDAOBondingCalculator;
     let redeemHelper;
-    let deploy = false;
+    let deploy = true;
+    let testnet = true;
     let SetGLBDVaultandMint10GLBD = true;
     let settersTestnet = true;
     let largeApproval = '100000000000000000000000000000000';
@@ -66,6 +67,61 @@ async function main() {
 
     // Quin bloc serà el primer que doni staking
     let firstBlockEpoch = await ethers.provider.getBlockNumber();
+
+    // SETUP AMM ENVIRONMENT
+    {
+        if (deploy && testnet) {
+            // Deploy factory
+            console.log("[Deploying factory SC]");
+            factory = await deployFactory(DEPLOYER_ADDRESS);
+            console.log("[Factory deployed]: " + factory.address + ".");
+            await new Promise(r => setTimeout(() => r(), 3000));
+        } else {
+            // Attach factory
+            console.log("[Attaching factory SC]");
+            const Factory = await ethers.getContractFactory("Factory");
+            factory = await Factory.attach(FACTORY_ADDRESS);
+            console.log("[Factory attached]: " + factory.address + ".");
+            await new Promise(r => setTimeout(() => r(), 1000));
+        }
+
+        if (deploy && testnet) {
+            // Deploy Router
+            console.log("[Deploying Router SC]");
+            router = await deployRouter(factory.address, WETH_ADDRESS); // Direcció WETH random només per tal de que funcioni el router.
+            console.log("[Router deployed]: " + router.address + ".");
+            await new Promise(r => setTimeout(() => r(), 3000));
+        } else {
+            // Attach Router
+            console.log("[Attaching Router SC]");
+            const Router = await ethers.getContractFactory("Router");
+            router = await Router.attach(ROUTER_BEGLOBAL_ADDRESS);
+            console.log("[Router attached]: " + router.address + ".");
+            await new Promise(r => setTimeout(() => r(), 1000));
+        }
+    }
+
+    if (deploy && testnet) {
+        // Deploy BUSD
+        console.log("[Deploying BUSD SC]");
+        busd = await BUSD.deploy();
+        console.log("[BUSDt deployed]: " + busd.address + ".");
+        await new Promise(r => setTimeout(() => r(), 3000));
+    } else {
+        // Attach BUSD
+        console.log("[Attaching BUSD SC]");
+        busd = await BUSD.attach(BUSD_ADDRESS);
+        console.log("[BUSDt attached]: " + busd.address + ".");
+        await new Promise(r => setTimeout(() => r(), 1000));
+    }
+
+    if (testnet) {
+        // Deployer mints 100 BUSD
+        console.log("[Deployer mints 100 BUSD]");
+        await busd.mint(INITIAL_SUPPLY_B);
+        await new Promise(r => setTimeout(() => r(), 3000));
+        console.log("[Success]");
+    }
 
     console.log('Deploying contracts. Deployer account: ' + deployer.address + '. Multisig account: ' + MULTISIG_ADDRESS + '.');
 
@@ -97,95 +153,13 @@ async function main() {
         await new Promise(r => setTimeout(() => r(), 1000));
     }
 
-    if (deploy) {
-        // Deploy factory
-        console.log("[Deploying factory SC]");
-        factory = await deployFactory(DEPLOYER_ADDRESS);
-        console.log("[Factory deployed]: " + factory.address + ".");
-        await new Promise(r => setTimeout(() => r(), 3000));
-    } else {
-        // Attach factory
-        console.log("[Attaching factory SC]");
-        const Factory = await ethers.getContractFactory("Factory");
-        factory = await Factory.attach(FACTORY_ADDRESS);
-        console.log("[Factory attached]: " + factory.address + ".");
-        await new Promise(r => setTimeout(() => r(), 1000));
-    }
-
-    // Set factory
-    console.log("[Setting factory]");
-    await factory.setFeeTo(TREASURY_SWAP_ADDRESS);
-    console.log("[Factory set]");
+    // Create GLBD-BUSD pair
+    console.log("[Create GLBD-BUSD pair]");
+    const createPair = await factory.createPair(GLBD.address, busd.address);
+    const createP = await createPair.wait();
+    GLBDBUSDLPADDRESS = createP.events[0].args.pair;
+    console.log("[Success, pair created]: " + GLBDBUSDLPADDRESS + ".");
     await new Promise(r => setTimeout(() => r(), 3000));
-
-    if (deploy) {
-        // Deploy Router
-        console.log("[Deploying Router SC]");
-        router = await deployRouter(factory.address, WETH_ADDRESS); // Direcció WETH random només per tal de que funcioni el router.
-        console.log("[Router deployed]: " + router.address + ".");
-        await new Promise(r => setTimeout(() => r(), 3000));
-    } else {
-        // Attach Router
-        console.log("[Attaching Router SC]");
-        const Router = await ethers.getContractFactory("Router");
-        router = await Router.attach(ROUTER_BEGLOBAL_ADDRESS);
-        console.log("[Router attached]: " + router.address + ".");
-        await new Promise(r => setTimeout(() => r(), 1000));
-    }
-
-    // Approve RouterBeGlobal as spender of GLB for Deployer
-    console.log("[Approve GLBD to be used in the BeGlobal router by the deployer]");
-    await GLBD.approve(router.address, largeApproval);
-    console.log("[Success]");
-    await new Promise(r => setTimeout(() => r(), 3000));
-
-    // set deployer as GLBD vault and mint 10 GLBD.
-    if (SetGLBDVaultandMint10GLBD)
-        await setGLBDVaultandMint10GLBD(GLBD);
-
-    if (deploy) {
-        // Deploy BUSD
-        console.log("[Deploying BUSD SC]");
-        busd = await BUSD.deploy();
-        console.log("[BUSDt deployed]: " + busd.address + ".");
-    } else {
-        // Attach BUSD
-        console.log("[Attaching BUSD SC]");
-        busd = await BUSD.attach(BUSD_ADDRESS);
-        console.log("[BUSDt attached]: " + busd.address + ".");
-    }
-
-
-    // Deployer mints 100 BUSD
-    console.log("[Deployer mints 100 BUSD]");
-    await busd.mint(INITIAL_SUPPLY_B);
-    await new Promise(r => setTimeout(() => r(), 3000));
-    console.log("[Success]");
-
-    // Approve BUSDt to be used in the BeGlobal router by the deployer
-    console.log("[Approve BUSDt to be used in the BeGlobal router by the deployer]");
-    await busd.approve(router.address, largeApproval);
-    console.log("[Success]");
-    await new Promise(r => setTimeout(() => r(), 3000));
-
-    // AddLiquidity
-    console.log("[Create and add liquidity GLBD-BUSD in BeGlobal router]");
-    const addLiq = await router.addLiquidity(
-        GLBD.address,
-        busd.address,
-        bep20Amount(1),
-        bep20Amount_B(1),
-        0,
-        0,
-        DEPLOYER_ADDRESS,
-        (new Date()).setTime((new Date()).getTime())
-    );
-    await new Promise(r => setTimeout(() => r(), 3000));
-
-    const r = await addLiq.wait();
-    let LP = r.events[1].topics[2];
-    GLBDBUSDLPADDRESS = LP.replace("000000000000000000000000", "");
-    console.log("[Success] GLBD-BUSD LP address: ", GLBDBUSDLPADDRESS, ".");
 
     if (deploy) {
         // Deploy treasury
@@ -216,6 +190,40 @@ async function main() {
         const Distributor = await ethers.getContractFactory('Distributor');
         distributor = await Distributor.attach(DISTRIBUTOR_ADDRESS);
         console.log("[Distributor attached]: " + distributor.address + ".");
+        await new Promise(r => setTimeout(() => r(), 1000));
+    }
+
+    if (deploy) {
+        // Deploy bonding calculator
+        console.log("[Deploy bonding calculator]");
+        const GlobalDAOBondingCalculator = await ethers.getContractFactory('GlobalDAOBondingCalculator');
+        globalDAOBondingCalculator = await GlobalDAOBondingCalculator.deploy(GLBD.address);
+        console.log("[GlobalDAOBondingCalculator deployed]: " + globalDAOBondingCalculator.address + ".");
+        await new Promise(r => setTimeout(() => r(), 3000));
+    } else {
+        // Attach bonding calculator
+        console.log("[Attaching bonding calculator]");
+        const GlobalDAOBondingCalculator = await ethers.getContractFactory('GlobalDAOBondingCalculator');
+        globalDAOBondingCalculator = await GlobalDAOBondingCalculator.attach(BONDING_CALCULATOR_ADDRESS);
+        console.log("[GlobalDAOBondingCalculator attached]: " + globalDAOBondingCalculator.address + ".");
+        await new Promise(r => setTimeout(() => r(), 1000));
+    }
+
+    //TODO deploy tokenBondDepository x 2
+
+    if (deploy) {
+        // Redeem helper
+        console.log("[Redeem helper]");
+        const RedeemHelper = await ethers.getContractFactory('RedeemHelper');
+        redeemHelper = await RedeemHelper.deploy();
+        console.log("[Redeem helper deployed]: " + redeemHelper.address + ".");
+        await new Promise(r => setTimeout(() => r(), 3000));
+    } else {
+        // Attach Redeem helper
+        console.log("[Attaching redeem helper]");
+        const RedeemHelper = await ethers.getContractFactory('RedeemHelper');
+        redeemHelper = await RedeemHelper.attach(REDEEM_HELPER_ADDRESS);
+        console.log("[Redeem helper attached]: " + redeemHelper.address + ".");
         await new Promise(r => setTimeout(() => r(), 1000));
     }
 
@@ -252,7 +260,6 @@ async function main() {
     }
 
     if (deploy) {
-
         // Deploy WarmUp
         console.log("[Deploy WarmUp]");
         const StakingWarmup = await ethers.getContractFactory('StakingWarmup');
@@ -268,37 +275,45 @@ async function main() {
         await new Promise(r => setTimeout(() => r(), 1000));
     }
 
-    if (deploy) {
-        // Deploy bonding calculator
-        console.log("[Deploy bonding calculator]");
-        const GlobalDAOBondingCalculator = await ethers.getContractFactory('GlobalDAOBondingCalculator');
-        globalDAOBondingCalculator = await GlobalDAOBondingCalculator.deploy(GLBD.address);
-        console.log("[GlobalDAOBondingCalculator deployed]: " + globalDAOBondingCalculator.address + ".");
-        await new Promise(r => setTimeout(() => r(), 3000));
-    } else {
-        // Attach bonding calculator
-        console.log("[Attaching bonding calculator]");
-        const GlobalDAOBondingCalculator = await ethers.getContractFactory('GlobalDAOBondingCalculator');
-        globalDAOBondingCalculator = await GlobalDAOBondingCalculator.attach(BONDING_CALCULATOR_ADDRESS);
-        console.log("[GlobalDAOBondingCalculator attached]: " + globalDAOBondingCalculator.address + ".");
-        await new Promise(r => setTimeout(() => r(), 1000));
-    }
+    // set deployer as GLBD vault and mint 10 GLBD.
+    if (SetGLBDVaultandMint10GLBD)
+        await setGLBDVaultandMint10GLBD(GLBD);
 
-    if (deploy) {
-        // Redeem helper
-        console.log("[Redeem helper]");
-        const RedeemHelper = await ethers.getContractFactory('RedeemHelper');
-        redeemHelper = await RedeemHelper.deploy();
-        console.log("[Redeem helper deployed]: " + redeemHelper.address + ".");
-        await new Promise(r => setTimeout(() => r(), 3000));
-    } else {
-        // Attach Redeem helper
-        console.log("[Attaching redeem helper]");
-        const RedeemHelper = await ethers.getContractFactory('RedeemHelper');
-        redeemHelper = await RedeemHelper.attach(REDEEM_HELPER_ADDRESS);
-        console.log("[Redeem helper attached]: " + redeemHelper.address + ".");
-        await new Promise(r => setTimeout(() => r(), 1000));
-    }
+
+
+
+
+    // Approve RouterBeGlobal as spender of GLB for Deployer
+    console.log("[Approve GLBD to be used in the BeGlobal router by the deployer]");
+    await GLBD.approve(router.address, largeApproval);
+    console.log("[Success]");
+    await new Promise(r => setTimeout(() => r(), 3000));
+
+
+    // Approve BUSDt to be used in the BeGlobal router by the deployer
+    console.log("[Approve BUSDt to be used in the BeGlobal router by the deployer]");
+    await busd.approve(router.address, largeApproval);
+    console.log("[Success]");
+    await new Promise(r => setTimeout(() => r(), 3000));
+
+    // AddLiquidity
+    console.log("[Create and add liquidity GLBD-BUSD in BeGlobal router]");
+    const addLiq = await router.addLiquidity(
+        GLBD.address,
+        busd.address,
+        bep20Amount(1),
+        bep20Amount_B(1),
+        0,
+        0,
+        DEPLOYER_ADDRESS,
+        (new Date()).setTime((new Date()).getTime())
+    );
+    await new Promise(r => setTimeout(() => r(), 3000));
+
+    const r = await addLiq.wait();
+    let LP = r.events[1].topics[2];
+    GLBDBUSDLPADDRESS = LP.replace("000000000000000000000000", "");
+    console.log("[Success] GLBD-BUSD LP address: ", GLBDBUSDLPADDRESS, ".");
 
     if (settersTestnet){
     // QUEUE, TOGGLE-..
